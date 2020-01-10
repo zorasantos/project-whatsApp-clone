f1cb6e3cb7d344ebdf5b06a38d95a3c071787d80
Zoranildo Santos <zoranildo22@gmail.com>
2020-01-09 21:35:59 -0300

    Ativando a câmera

---
+++ src/controller/CameraController.js
@@ -0,0 +1,21 @@
+class CameraController {
+
+    constructor(videoEl) {
+
+        this._videoEl = videoEl;
+
+        navigator.mediaDevices.getUserMedia({
+            video: true
+        }).then(stream => {
+
+            this._stream = stream;
+            this._videoEl.srcObject = stream;
+            this._videoEl.play();
+
+        }).catch(err => {
+            console.error(err);
+        })
+
+    }
+
+}
