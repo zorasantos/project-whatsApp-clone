342e9fd8021892a6cd59089442d4bcc04da91129
Zoranildo Santos <zoranildo22@gmail.com>
2020-01-14 22:03:55 -0300

    Enviando mensagens de documentos anexos

--- src/controller/WhatsAppController.js
+++ src/controller/WhatsAppController.js
@@ -1,5 +1,6 @@
 import {Format} from '../util/Format';
 import {CameraController} from './CameraController'; 
+import {DocumentPreviewController} from './DocumentPreviewController';
  
  export class WhatsAppController {
 
@@ -244,7 +245,57 @@ import {CameraController} from './CameraController';
             this.el.panelDocumentPreview.css({
                 'height': '100%'
             });
+            this.el.inputDocument.click();
         });
+
+        this.el.inputDocument.on('change', e => {
+
+            if (this.el.inputDocument.files.length) {
+
+                let file = this.el.inputDocument.files[0];
+
+                this._documentPreviewController = new DocumentPreviewController(file);
+
+                this._documentPreviewController.getPreviewData().then(result => {
+
+                    this.el.imgPanelDocumentPreview.src = result.src;
+                    this.el.infoPanelDocumentPreview.innerHTML = result.info;
+                    this.el.imagePanelDocumentPreview.show();
+                    this.el.filePanelDocumentPreview.hide();
+
+                }).catch(err => {
+
+                    switch(file.type) {
+
+                        case 'application/vnd.ms-excel':
+                        case 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet':
+                            this.el.iconPanelDocumentPreview.className = 'jcxhw icon-doc-xls';
+                        break;
+
+                        case 'application/vnd.ms-powerpoint':
+                        case 'application/vnd.openxmlformats-officedocument.presentationml.presentationml':
+                            this.el.iconPanelDocumentPreview.className = 'jcxhw icon-doc-ppt';
+                        break;
+
+                        case 'application/msword':
+                        case 'application/vnd.openxmlformats-officedocument.wordprocessingml.document':
+                            this.el.iconPanelDocumentPreview.className = 'jcxhw icon-doc-doc';
+                        break;
+
+                        default:
+                            this.el.iconPanelDocumentPreview.className = 'jcxhw icon-doc-generic';
+                        break;
+                    }
+                    this.el.filenamePanelDocumentPreview.innerHTML = file.name;
+                    this.el.imagePanelDocumentPreview.hide();
+                    this.el.filePanelDocumentPreview.show();
+
+                })
+                
+
+            }
+
+        })
 
         this.el.btnClosePanelDocumentPreview.on('click', e => {
 
